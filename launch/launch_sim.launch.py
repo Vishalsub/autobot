291843d2e6ead57af5c07b1fb1ca13d50972c82e
Vishalsub <vishalsubra127@gmail.com>
2024-01-25 12:22:07 +0530

    add ros2_control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,21 +1,14 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
 from launch_ros.actions import Node
 
 
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
     package_name='autobot' 
 
@@ -37,6 +30,21 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+    
+
+
 
 
     # Launch them all!
@@ -44,4 +52,7 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
-    ])
+        diff_drive_spawner,
+        joint_broad_spawner
+
+  ])
